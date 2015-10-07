59b159b1eaa345c0aca54ee7bdb11812520dbd3d
Robert S Crawford <craw.rs@gmail.com>
2015-10-07 12:05:09 -0700

    Successfully passes all tests.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,9 +7,15 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    if (length > 0) { length--; }
+    var temp = storage[length];
+    delete storage[length];
+    return temp;
   };
 
   someInstance.size = function() {
