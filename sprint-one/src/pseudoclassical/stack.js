fa07077bb14f013c475d08f16f44cdc1c3c08ba8
sdwang <siwang22@gmail.com>
2015-10-07 16:09:12 -0700

    Completed stack.js using pseudoclassical style.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
 
+ Stack.prototype.size = function() {
+  return this.length;
+ }
 
+ Stack.prototype.push = function(value) {
+  this.length++;
+  this[this.length] = value;
+ }
+
+ Stack.prototype.pop = function() {
+  var temp;
+  if (this.length > 0) {
+    temp = this[this.length];
+    delete this[this.length];
+    this.length--;
+  }
+  return temp;
+ }
