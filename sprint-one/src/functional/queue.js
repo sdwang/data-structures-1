fb428ff00d268b4b92e74597c2c1978fe71ae385
Robert S Crawford <craw.rs@gmail.com>
2015-10-07 13:29:12 -0700

    Tests successfully pass. Was missing that as values are removed and length is decremented, we must increment queue by the same amount, otherwise key-value pairs in storage will be overwritten.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,27 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var length = 0;
+  var queue = 0;
+  var temp;
 
   someInstance.enqueue = function(value) {
+    length++;
+    storage[length+queue] = value;
   };
 
   someInstance.dequeue = function() {
+    if (length > 0) { 
+      length--;
+    }
+    queue++;
+    temp = storage[queue];
+    delete storage[queue];
+    return temp;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
