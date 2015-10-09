3c814af4d57d2065ccb2041a3d6706392f66e512
sdwang <siwang22@gmail.com>
2015-10-09 11:28:49 -0700

    Implemented hashTable.js using pseudoclassical style.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,33 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if ( this._storage[index] === undefined ) { this._storage[index] = []; }
+  var bucket = this._storage[index];
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[i][0] === k ) { var place = bucket[i]; }
+  }
+  if ( place ) {
+    place[1] = v;
+  } else {
+    this._storage[index].push( [k, v] );
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[i] === undefined ) { return null; }
+    if ( bucket[i][0] === k ) { return bucket[i][1]; }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[i][0] === k ) { bucket[i] = undefined; }
+  }
 };
 
 
